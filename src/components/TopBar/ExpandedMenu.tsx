4de27c54275b9e0010ebaa8eaf8988a8edc082fb
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2021-04-11 21:55:28 +0200

    początek zabawy z expandedMenu

--- src/components/TopBar/ExpandedMenu.tsx
+++ src/components/TopBar/ExpandedMenu.tsx
@@ -1,7 +1,29 @@
 import { FC } from 'React';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    position: absolute;
+    width: 240px;
+    top: 24px;
+    background: rgba(143, 143, 143, 0.5);
+    padding: 10px;
+    z-index:1;
+
+    li{
+        padding: 5px;
+    }
+`;
 
 export const ExpandedMenu: FC = () => {
     return (
-        <>ExpandedMenu</>
+        <Wrapper>
+            <ul>
+                <li>Text 1</li>
+                <li>Text 2</li>
+                <li>Text 3</li>
+                <li>Text 4</li>
+                <li>Text 5</li>
+            </ul>
+        </Wrapper>
     );
 };
