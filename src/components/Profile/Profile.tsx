e7d6b5707ec50c1915f4da725bd2f110640dea7b
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2021-06-09 23:20:19 +0200

    dodano nieco styli i danych do profile.tsx

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useEffect} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import styled from 'styled-components';
 
 import {fontSize} from '../../styledHelpers/FontSizes';
@@ -14,9 +14,72 @@ import { IImageReducer } from '../../reducers/imageReducers';
 type GetImg = ReturnType<typeof getImg>
 type GetUsers = ReturnType<typeof getUsers>
 
-const ProfileWrapper = styled.div``;
+const ProfileWrapper = styled.div`
+    position: absolute;
+    margin-top: 680px;
+    margin-left: 320px;
+    width: 870px;
+    height: 1000px;
+`;
 
 export const Profile: FC = () => {
+
+    const ProfileTopBarInfo = styled.div`
+
+            width: 870px;
+            height: 35px;
+
+            .buttons{
+                float: right;
+            }
+
+            button{
+                height: 30px;
+                margin: 0 5px;
+                background-color: transparent;
+                color: ${Colors.navy_blue};
+                font-size: ${fontSize[16]};
+                border: none;
+                font-weight: 600;
+            }
+
+            img{
+                height: 16px;
+            }
+        `;
+
+    const ProfileDescription = styled.div`
+        background-color: green;
+        width: 870px;
+        height: 140px;
+        display: grid;
+        grid-template-columns: 1fr 2fr 2fr;
+
+        .profilePhotoSection{
+            display: grid;
+            grid-column: 1;
+            padding: 10px;
+            img{
+                border-radius: 50%;
+                height: 90px;
+                margin-bottom: 5px;
+            }
+
+            button{
+                width: 95px;
+                background-color: transparent;
+                color: ${Colors.text_color};
+                border: none;
+                font-size: ${fontSize[16]};
+            }
+        }
+
+        .profileDescriptionSecction{
+
+        }
+    `;
+
+    const [company, setCompany] = useState('');
 
     const {usersList } = useSelector<IState, IUsersReducer>(state => ({
         ...state.users
@@ -32,9 +95,31 @@ export const Profile: FC = () => {
         dispatch<GetUsers>(getUsers());
     }, [dispatch]);
 
+    useEffect(()=>{
+        setCompany(usersList[0]?.company.name);
+    },[usersList])
+
     return(
         <ProfileWrapper>
+            <ProfileTopBarInfo>
+                <div className="buttons">
+                    <button><img src="./media/icons/comments.svg" alt="message"></img> Message</button>
+                    <button><img src="./media/icons/plus.svg" alt="plus"></img> Create a request</button>
+                    <button><img src="./media/icons/administration.svg" alt="cluster"></img> Add to cluster</button>
+                </div>
+            </ProfileTopBarInfo>
+            <ProfileDescription>
+                <div className="profilePhotoSection">
+                    <img src={imageList[0]?.thumbnailUrl} alt=""></img>
+                    <button>See profile</button>
+                </div>
+                <div className="profileDescriptionSecction">
+                    <p>{company}</p>
+                </div>
+                <div className="profileCommunicateSecction">
 
+                </div>
+            </ProfileDescription>
         </ProfileWrapper>
     )
 
